e864c614fb27ca6348ac219299f7e502313d1d55
Janin-Michel-Mathias <micheljanin296@gmail.com>
2024-01-24 11:52:33 +0100

    add green cases for skill class

---
+++ src/test/java/SkillTests.java
@@ -0,0 +1,16 @@
+import modeles.Skill;
+import org.junit.Test;
+
+public class Tests {
+    String skillName = "Node";
+    String skillLevel = "Expert";
+    @BeforeEach
+
+    @Test
+    public void shouldCreateASkill() {
+        assert(s.getSkillName()).equals(skillName);
+        assert(s.getLevel()).equals(skillLevel);
+    }
+
+    public
+}
